c281e65a6d69801aaa19829c5d7c5a78c4dc1bb9
Louis L <louis.leculier@gmail.com>
2024-03-13 18:20:12 +0100

    test

--- components/connected_bedroom/connected_bedroom.cpp
+++ components/connected_bedroom/connected_bedroom.cpp
@@ -59,11 +59,8 @@ int getIntFromString(String &string, int position, int lenght) {
 void ConnectedBedroom::loop() {
   // Receive and process messages.
   while (this->available()) {
-    int letter = this->read();
-    ESP_LOGD(TAG, "Received letter INT: %i", letter);
-    ESP_LOGD(TAG, "Received letter CHAR: %c", char(letter));
-    if (letter == -1)
-      continue;
+    char letter = this->read();
+    ESP_LOGD(TAG, "Message: %s", this->receivedMessage_);
     if (letter == '\r')
       continue;
     if (letter == '\n')
