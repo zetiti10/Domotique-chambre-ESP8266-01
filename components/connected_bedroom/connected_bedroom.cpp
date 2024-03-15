22404c8ccb082430626cb46bc3bbd4287e4afe97
Louis L <louis.leculier@gmail.com>
2024-03-15 18:12:54 +0100

    Test

--- components/connected_bedroom/connected_bedroom.cpp
+++ components/connected_bedroom/connected_bedroom.cpp
@@ -177,11 +177,11 @@ void ConnectedBedroomSwitch::set_parent(ConnectedBedroom *parent) {
 }
 
 void ConnectedBedroomSwitch::write_state(bool state) {
-  this->parent_->write(0);
+  this->parent_->write('0');
   this->parent_->write_str(addZeros(this->communication_id_, 2).c_str());
-  this->parent_->write(0);
-  this->parent_->write(0);
-  this->parent_->write(state);
+  this->parent_->write('0');
+  this->parent_->write('0');
+  this->parent_->write((char)state);
   this->parent_->write('\n');
 }
 
