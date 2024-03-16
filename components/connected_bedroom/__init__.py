565d65de255cde146a517e1dbdd3fc5a15384b7c
Louis L <louis.leculier@gmail.com>
2024-03-16 17:26:41 +0100

    Test

--- components/connected_bedroom/__init__.py
+++ components/connected_bedroom/__init__.py
@@ -61,7 +61,7 @@ CONFIG_SCHEMA = uart.UART_DEVICE_SCHEMA.extend(
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
 
